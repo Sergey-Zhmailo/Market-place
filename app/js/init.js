a32df8ab7e0bcd017de1800ae39bd0f2a89f784a
emmattfu <makcimdjan@gmail.com>
2018-04-20 16:31:17 +0300

    all new items 20/04

--- app/js/init.js
+++ app/js/init.js
@@ -16,10 +16,15 @@ $('.grid-thumbs').on('click', function(){
     // Find cards and remove class row-card
     $('.all-items-container .card').removeClass('row-card');
     $('.all-items-container .col-4').removeClass('col-12');
+    $('.sort-controls .grid-thumbs').addClass('active');
+    $('.sort-controls .grid-list').removeClass('active');
 });
 
 $('.grid-list').on('click', function(){
     // Find cards and add class row-card
     $('.all-items-container .card').addClass('row-card');
     $('.all-items-container .col-4').addClass('col-12');
+    $('.sort-controls .grid-thumbs').removeClass('active');
+    $('.sort-controls .grid-list').addClass('active');
 });
+
