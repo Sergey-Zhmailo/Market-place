f38e1ecf8c64a4183fca9e4a1f9384fbeffba18c
Sergey Zhmailo <sergey.seoz@gmail.com>
2018-04-27 12:21:28 +0300

    адаптив HOME (без тегов, карточек, поиска)

--- app/js/init.js
+++ app/js/init.js
@@ -5,7 +5,31 @@ $( function() {
         slidesToShow: 4,
         slidesToScroll: 1,
         dots: true,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+
+        ]
     });
 } );
 
